28ff861cf2cbfa64ab5e8555d95f8e5fa88f2e54
Alexandre Lopes <alexbarreiraneto@hotmail.com>
2026-08-08 11:56:54 -0300

    hg-painel-v1.4.3 Add Prisma client import to ensure proper packaging on Vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 const { createRequestHandler } = require("@expo/server/adapter/vercel");
 
+// Força o Vercel a empacotar o Prisma
+require("@prisma/client");
+
 module.exports = createRequestHandler({
   build: require("path").join(__dirname, "../dist/server"),
 });
